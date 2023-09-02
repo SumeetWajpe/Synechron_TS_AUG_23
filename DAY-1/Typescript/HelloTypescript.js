ace9a13f530b2711701f3ccad904f3966de39c3d
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-02 10:14:27 +0530

    ✨ Type annotation

--- DAY-1/Typescript/HelloTypescript.js
+++ DAY-1/Typescript/HelloTypescript.js
@@ -1,2 +1,14 @@
-var x = 100;
-x = "Hello !";
+// var x = 100;
+// x = "Hello !";
+var x = 100; //Type Inferencing
+var s = "Hello !";
+var y = 200; // Type annotation
+var b;
+b = true;
+console.log(b);
+var str;
+var z;
+z = 10;
+z = "Hi";
+z = [10, 20];
+z = { x: 100 };
--- DAY-1/Typescript/HelloTypescript.ts
+++ DAY-1/Typescript/HelloTypescript.ts
@@ -4,3 +4,12 @@
 var x = 100; //Type Inferencing
 var s = "Hello !";
 var y: number = 200; // Type annotation
+var b: boolean;
+b = true;
+console.log(b);
+var str: string;
+var z;
+z = 10;
+z = "Hi";
+z = [10, 20];
+z = { x: 100 };
