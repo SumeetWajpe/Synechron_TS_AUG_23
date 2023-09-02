7907b4e9e62ba756361a9cc830dbf394da8b2816
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-02 10:56:56 +0530

    ✨ using let & const

--- DAY-1/Typescript/HelloTypescript.js
+++ DAY-1/Typescript/HelloTypescript.js
@@ -12,3 +12,27 @@ z = 10;
 z = "Hi";
 z = [10, 20];
 z = { x: 100 };
+// Arrays
+//var cars = ["BMW", "AUDI", "MERC"]; // Type Inferencing
+// OR
+//var cars: string[] = ["BMW", "AUDI", "MERC"]; // Type Annotation
+//var cars = new Array("BMW", "AUDI");
+// OR
+// Using Generics
+var cars = new Array("BMW", "AUDI", "MERC");
+for (var _i = 0, cars_1 = cars; _i < cars_1.length; _i++) {
+    var car = cars_1[_i];
+    console.log(car);
+}
+if (true) {
+    var q = void 0;
+    q = 100;
+    console.log(q);
+    // after 100 lines
+    //   let q = 200; // Error !
+}
+// const PI: number = 3.14;
+// PI = 3.1434; // Error !
+var player = { name: "Neeraj Chopra", country: "INDIA" };
+// player = {};// Error !
+player.country = "Bharat";
--- DAY-1/Typescript/HelloTypescript.ts
+++ DAY-1/Typescript/HelloTypescript.ts
@@ -13,3 +13,32 @@ z = 10;
 z = "Hi";
 z = [10, 20];
 z = { x: 100 };
+
+// Arrays
+//var cars = ["BMW", "AUDI", "MERC"]; // Type Inferencing
+// OR
+//var cars: string[] = ["BMW", "AUDI", "MERC"]; // Type Annotation
+
+//var cars = new Array("BMW", "AUDI");
+// OR
+// Using Generics
+var cars: Array<string> = new Array<string>("BMW", "AUDI", "MERC");
+
+for (const car of cars) {
+  console.log(car);
+}
+
+if (true) {
+  let q;
+  q = 100;
+  console.log(q);
+  // after 100 lines
+  //   let q = 200; // Error !
+}
+
+// const PI: number = 3.14;
+// PI = 3.1434; // Error !
+
+const player = { name: "Neeraj Chopra", country: "INDIA" };
+// player = {};// Error !
+player.country = "Bharat";
