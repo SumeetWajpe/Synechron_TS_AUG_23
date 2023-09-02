02563282ea69f55b15202bf6146572f05f17a30f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-02 16:04:16 +0530

    ✨ Class Basics

--- DAY-1/Typescript/OOP.js
+++ DAY-1/Typescript/OOP.js
@@ -6,10 +6,31 @@ var Designation;
     Designation[Designation["Architect"] = 400] = "Architect";
 })(Designation || (Designation = {}));
 var d = Designation.Tester;
-console.log(d);
-console.log(Designation[d]);
+console.log(d); // 200
+console.log(Designation[d]); // Tester
 // class Emp {
 //   designation: Designation;
 // }
 // var e = new Emp();
 // e.designation = Designation.Tester
+// write an array of movies[5] - title, budget, threatre, category (Enum)
+// write a method GetMoviesByCategory(category)
+var MovieCategory;
+(function (MovieCategory) {
+    MovieCategory[MovieCategory["Fiction"] = 0] = "Fiction";
+    MovieCategory[MovieCategory["Romance"] = 1] = "Romance";
+    MovieCategory[MovieCategory["Action"] = 2] = "Action";
+})(MovieCategory || (MovieCategory = {}));
+var Movie = /** @class */ (function () {
+    function Movie(title, budget, category) {
+        if (title === void 0) { title = "Unknown"; }
+        if (budget === void 0) { budget = 0; }
+        if (category === void 0) { category = MovieCategory.Action; }
+        this.title = title;
+        this.budget = budget;
+        this.category = category;
+    }
+    return Movie;
+}());
+var movieObj = new Movie();
+console.log(movieObj.title);
--- DAY-1/Typescript/OOP.ts
+++ DAY-1/Typescript/OOP.ts
@@ -14,3 +14,40 @@ console.log(Designation[d]); // Tester
 
 // var e = new Emp();
 // e.designation = Designation.Tester
+
+// write an array of movies[5] - title, budget, threatre, category (Enum)
+// write a method GetMoviesByCategory(category)
+enum MovieCategory {
+  Fiction,
+  Romance,
+  Action,
+}
+
+class Movie {
+  title: string;
+  budget: number;
+  category: MovieCategory;
+
+  constructor(
+    title: string = "Unknown",
+    budget: number = 0,
+    category: MovieCategory = MovieCategory.Action,
+  ) {
+    this.title = title;
+    this.budget = budget;
+    this.category = category;
+  }
+}
+
+// var movieObj = new Movie();
+// console.log(movieObj.title);
+
+// let movies = [
+//   new Movie("Sholay", 100000, MovieCategory.Action),
+//   new Movie("Swades", 1000000, MovieCategory.Fiction),
+// ];
+// OR
+let movies: Array<Movie> = new Array<Movie>(
+  new Movie("Sholay", 100000, MovieCategory.Action),
+  new Movie("Swades", 1000000, MovieCategory.Fiction),
+);
