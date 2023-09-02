c0c50c6171807c1cddc570f5742fc99ab43498fc
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-02 11:36:45 +0530

    ✨ Optional Parameters

--- DAY-1/Typescript/Functions.js
+++ DAY-1/Typescript/Functions.js
@@ -3,9 +3,25 @@
 // }
 // console.log(Add(20, 30));
 // Add();// Error !
-function Add(x, y) {
-    return x + y;
-}
-var result = Add(10, 20);
-console.log(result);
+// function Add(x: number, y: number): number | string {
+//   if (x == 0) {
+//     return "X Can't be zero !";
+//   }
+//   return x + y;
+// }
+// let result: number | string = Add(10, 20);
+// console.log(result);
 // Add("Hello", "World !");
+// let d: number | string;
+// d = 10;
+// d = "Hi";
+// d = true;
+// function Square(x: number): void {
+//   return 100;
+// }
+// Optional Parameters
+function PrintBook(author, title, noOfPages) {
+    console.log(author, title, noOfPages);
+}
+PrintBook("Dr. APJ Abdul Kalam", "Wings Of Fire");
+PrintBook("Dr. APJ Abdul Kalam", "Wings Of Fire", 300);
--- DAY-1/Typescript/Functions.ts
+++ DAY-1/Typescript/Functions.ts
@@ -4,11 +4,32 @@
 // console.log(Add(20, 30));
 // Add();// Error !
 
-function Add(x: number, y: number): number {
-  return x + y;
+// function Add(x: number, y: number): number | string {
+//   if (x == 0) {
+//     return "X Can't be zero !";
+//   }
+//   return x + y;
+// }
+
+// let result: number | string = Add(10, 20);
+
+// console.log(result);
+// Add("Hello", "World !");
+
+// let d: number | string;
+// d = 10;
+// d = "Hi";
+// d = true;
+
+// function Square(x: number): void {
+//   return 100;
+// }
+
+// Optional Parameters
+
+function PrintBook(author: string, title: string, noOfPages?: number) {
+  console.log(author, title, noOfPages);
 }
 
-let result: number = Add(10, 20);
-
-console.log(result);
-// Add("Hello", "World !");
+PrintBook("Dr. APJ Abdul Kalam", "Wings Of Fire");
+PrintBook("Dr. APJ Abdul Kalam", "Wings Of Fire", 300);
